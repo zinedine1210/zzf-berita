cd62fa62b3d413d5b6de9ffd29ec1cc88a56e6dc
zinedine1210 <ziddanfhdlvy12@gmail.com>
2022-06-22 16:40:51 +0700

    redux devtools

--- store/store.js
+++ store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import { createWrapper } from "next-redux-wrapper";
 import rootReducer from './reducers/rootReducers'
 
@@ -14,7 +14,7 @@ const middleware = [thunk];
 export const store = createStore(
   rootReducer,
   initalState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 // assigning store to next wrapper
